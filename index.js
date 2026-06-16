7a8d08ade7280f2752990ec3d6fe16850ee0a935
Rathallist <111634906+Rathallist@users.noreply.github.com>
2026-06-16 09:56:47 +0000

    première test

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ async function getData() {
   try {
     const response = await fetch('https://localhost3000/api/player/random');
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const random.name = await response.json();
-    console.log(random.name);
+    const random = await response.json();
+    console.log(player["name"]);
   } catch (error) {
     console.error('Fetch failed:', error);
   }
@@ -13,8 +13,8 @@ async function getData() {
   try {
     const response = await fetch('https://localhost3000/api/player/random');
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const random.team = await response.json();
-    console.log(random.team);
+    const random = await response.json();
+    console.log(response[question]);
   } catch (error) {
     console.error('Fetch failed:', error);
   }
@@ -24,41 +24,34 @@ async function getData() {
   try {
     const response = await fetch('https://localhost3000/api/player/random');
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const random.question = await response.json();
-    console.log(random.question);
+    const random = await response.json();
+    console.log(player[image]);
   } catch (error) {
     console.error('Fetch failed:', error);
   }
 }
 
-async function getData() {
-  try {
-    const response = await fetch('https://localhost3000/api/player/random');
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const random.image = await response.json();
-    console.log(random.image);
-  } catch (error) {
-    console.error('Fetch failed:', error);
-  }
-}
+Name = player["name"]
+let elementHTML = document.getElementById("name");
+elementHTML.textContent = donnee;
 
-Name = random.name
-team = random.team
-question = random.question
-image = random.iamge
+question = response["question"]
+let elementHTML = document.getElementById("question");
+elementHTML.textContent = donnee;
+
+image = player["image"]
+let elementHTML = document.getElementById("image");
+elementHTML.textContent = hurl;
 
 if (!joueur)
-    return res.status(404).json({"message":"Pas de joueur"});
-
-
-    
+  return res.status(404).json({"message":"Pas de joueur"}); 
 
 // --- Traitement du formulaire ---
 if (request.method == "Post")
-    team = request.form.get("team")
+  team = request.form.get("team")
 
 // --- Vérifier que les données sont bonnes ---
 if (joueur["team"] != team)
-    return res.status(404).json({"message":"Wrong Team"});
+  return res.status(404).json({"message":"Wrong Team"});
 else
-    return res.status(200).json({"message":"Bonne réponse"})
+  return res.status(200).json({"message":"Bonne réponse"})
