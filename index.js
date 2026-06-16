e6e57091108a9ae16f2f8b2dd822ac1767cd0f30
Xenon99o9 <liampaulus244@gmail.com>
2026-06-16 10:11:09 +0000

    pas OK mais on fait avec

--- index.js
+++ index.js
@@ -1,57 +1,43 @@
+const player = {}
+
+getData()
+
 async function getData() {
   try {
-    const response = await fetch('https://localhost3000/api/player/random');
+    const response = await fetch('http://127.0.0.1:3000/api/player/random');
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const random = await response.json();
-    console.log(player["name"]);
+    //const random = await response.json();
+    console.log(reponse)
+
   } catch (error) {
     console.error('Fetch failed:', error);
   }
 }
 
-async function getData() {
-  try {
-    const response = await fetch('https://localhost3000/api/player/random');
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const random = await response.json();
-    console.log(response[question]);
-  } catch (error) {
-    console.error('Fetch failed:', error);
-  }
-}
-
-async function getData() {
-  try {
-    const response = await fetch('https://localhost3000/api/player/random');
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    const random = await response.json();
-    console.log(player[image]);
-  } catch (error) {
-    console.error('Fetch failed:', error);
-  }
-}
 
 Name = player["name"]
-let elementHTML = document.getElementById("name");
-elementHTML.textContent = donnee;
+let nameHtml = document.getElementById("name");
+nameHtml.textContent = Name;
+
+
 
 question = response["question"]
-let elementHTML = document.getElementById("question");
-elementHTML.textContent = donnee;
+let questionHtml = document.getElementById("question");
+questionHtml.textContent = question;
 
 image = player["image"]
-let elementHTML = document.getElementById("image");
-elementHTML.textContent = hurl;
+let imageHtml = document.getElementById("image");
+imageHtml.textContent = image;
 
-if (!joueur)
-  return res.status(404).json({"message":"Pas de joueur"}); 
+//if (!joueur)
+//  return res.status(404).json({"message":"Pas de joueur"}); 
 
 // --- Traitement du formulaire ---
-if (request.method == "Post")
-  team = request.form.get("team")
+//if (request.method == "Post")
+//  team = request.form.get("team")
 
 // --- Vérifier que les données sont bonnes ---
-if (joueur["team"] != team)
-  return res.status(404).json({"message":"Wrong Team"});
-else
-  return res.status(200).json({"message":"Bonne réponse"})
+//if (joueur["team"] != team)
+//  return res.status(404).json({"message":"Wrong Team"});
+//else
+//  return res.status(200).json({"message":"Bonne réponse"})
